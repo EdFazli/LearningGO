9e39a1dcd36b7575a5ca304304835b56495aaf3e
EdFazli <edfazli92@gmail.com>
2021-11-20 04:00:00 +0800

    adding using map as a set

--- ch3/compositeTypes.go
+++ ch3/compositeTypes.go
@@ -195,4 +195,17 @@ func main() {
 	}
 	delete(ee, "goodbye")
 
+	//using map as a set
+	intSet := map[int]bool{}
+	vals := []int{5, 10, 2, 5, 8, 7, 3, 9, 1, 2, 10}
+	for _, xxx := range vals {
+		intSet[xxx] = true
+	}
+	fmt.Println(len(vals), len(intSet)) // 11 8
+	fmt.Println(intSet[5])              // true
+	fmt.Println(intSet[500])            // false
+	if intSet[100] {
+		fmt.Println("100 is in the set")
+	}
+
 }
