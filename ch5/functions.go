33dcca3657237eb2f764261d6bf5b5453b196599
EdFazli <edfazli92@gmail.com>
2021-11-21 04:18:32 +0800

    adding variadic function named addTo

--- ch5/functions.go
+++ ch5/functions.go
@@ -26,6 +26,14 @@ func main() {
 		LastName:  "John",
 	})
 
+	//different ways to call variadic function named addTo
+	fmt.Println(addTo(3))             // []
+	fmt.Println(addTo(3, 2))          // [5]
+	fmt.Println(addTo(3, 2, 4, 6, 8)) // [5 7 9 11]
+	a := []int{4, 3}
+	fmt.Println(addTo(3, a...))                    // [7 6]
+	fmt.Println(addTo(3, []int{1, 2, 3, 4, 5}...)) // [4 5 6 7 8]
+
 }
 
 //declaring function named div
@@ -38,4 +46,13 @@ func div(numerator int, denumerator int) int {
 
 //using struct to simulate named parameters
 func MyFunc(opts MyFuncOpts) {
+}
+
+//variadic function named addTo
+func addTo(base int, vals ...int) []int {
+	out := make([]int, 0, len(vals))
+	for _, v := range vals {
+		out = append(out, base+v)
+	}
+	return out
 }
