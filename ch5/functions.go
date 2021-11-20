e1386b9d53c01179f39983fc91d19306480e85f2
EdFazli <edfazli92@gmail.com>
2021-11-21 03:37:55 +0800

    adding function declaration

--- ch5/functions.go
+++ ch5/functions.go
@@ -6,7 +6,14 @@ import (
 
 func main() {
 	fmt.Println("This is functions examples")
+	result := div(5, 2)
+	fmt.Println(result)
+}
 
-	//
-
+//declaring function named div
+func div(numerator int, denumerator int) int {
+	if denumerator == 0 {
+		return 0
+	}
+	return numerator / denumerator
 }
