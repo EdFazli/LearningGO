ae35fb003d896fbc0c00dcdb619abf85bdfd1b2e
EdFazli <edfazli92@gmail.com>
2021-11-27 02:47:08 +0800

    adding pointer declaration

--- ch6/pointers.go
+++ ch6/pointers.go
@@ -7,4 +7,12 @@ import (
 func main() {
 	fmt.Println("This is pointers examples")
 
+	//pointer declaration
+	x := 10
+	pointerToX := &x
+	fmt.Println(pointerToX)  // prints memory address - 0xc000012088
+	fmt.Println(*pointerToX) // prints value to that memory address - 10
+	z := 5 + *pointerToX
+	fmt.Println(z) // 15
+
 }
