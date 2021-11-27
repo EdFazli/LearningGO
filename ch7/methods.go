0c0dc8afb2858d3a5b116089dc73ce31b6abec56
EdFazli <edfazli92@gmail.com>
2021-11-28 02:23:24 +0800

    adding methods

--- ch7/methods.go
+++ ch7/methods.go
@@ -2,9 +2,55 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+
+type Person struct {
+	firstName string
+	lastName  string
+	age       int
+}
+
+type Counter struct {
+	total       int
+	lastUpdated time.Time
+}
+
+//Methods declaration
+func (p Person) String() string {
+	return fmt.Sprintf("%s %s, age %d", p.firstName, p.lastName, p.age)
+}
+
+//pointer receivers and value receivers
+func (c *Counter) Increment() {
+	c.total++
+	c.lastUpdated = time.Now()
+}
+
+func (c Counter) String() string {
+	return fmt.Sprintf("total: %d, last updated: %v", c.total, c.lastUpdated)
+}
+
+//>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
 
 func main() {
 	fmt.Println("This is types, methods and interfaces examples")
 
+	//Methods invocation
+	p := Person{
+		firstName: "Dave",
+		lastName:  "John",
+		age:       24,
+	}
+	output := p.String()
+	fmt.Println(output) // Dave John, age 24
+
+	//pointer receivers and value receivers
+	var c Counter
+	fmt.Println(c.String()) // total: 0, last updated: 0001-01-01 00:00:00 +0000 UTC
+	c.Increment()
+	fmt.Println(c.String()) // total: 1, last updated: 2021-11-28 02:14:28.0163101 +0800 +08 m=+0.004450801
+
 }
+
+//>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
