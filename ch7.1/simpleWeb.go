62ea87c3d9bc90d75deb94a9448b1fe6e03720f3
EdFazli <edfazli92@gmail.com>
2021-12-05 00:56:33 +0800

    adding dependency injection example

--- ch7.1/simpleWeb.go
+++ ch7.1/simpleWeb.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
 )
 
 //>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
@@ -9,6 +11,116 @@ import (
 func main() {
 	fmt.Println("This is dependency injections example ")
 
+	l := LoggerAdapter(LogOutput)
+	ds := NewSimpleDataStore()
+	logic := NewSimpleLogic(l, ds)
+	c := NewController(l, logic)
+	http.HandleFunc("/hello", c.SayHello)
+	http.ListenAndServe(":8080", nil)
 }
 
 //>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
+
+//logger
+func LogOutput(message string) {
+	fmt.Println(message)
+}
+
+//data store
+type SimpleDataStore struct {
+	userData map[string]string
+}
+
+func (sds SimpleDataStore) UserNameForID(userID string) (string, bool) {
+	name, ok := sds.userData[userID]
+	return name, ok
+}
+
+//factory function to create instance of SimpleDataStore
+func NewSimpleDataStore() SimpleDataStore {
+	return SimpleDataStore{
+		userData: map[string]string{
+			"1": "Fred",
+			"2": "Mary",
+			"3": "Pat",
+		},
+	}
+}
+
+//interfaces to describe what it depends on
+type DataStore interface {
+	UserNameForID(userID string) (string, bool)
+}
+
+type Logger interface {
+	Log(message string)
+}
+
+//to make LogOutput function meets this interface, define function type with a method on it
+type LoggerAdapter func(message string)
+
+func (lg LoggerAdapter) Log(message string) {
+	lg(message)
+}
+
+//business logic
+type SimpleLogic struct {
+	l  Logger
+	ds DataStore
+}
+
+func (sl SimpleLogic) SayHello(userID string) (string, error) {
+	sl.l.Log("in SayHello for " + userID)
+	name, ok := sl.ds.UserNameForID(userID)
+	if !ok {
+		return "", errors.New("unknown user")
+	}
+	return "Hello, " + name, nil
+}
+
+func (sl SimpleLogic) SayGoodbye(userID string) (string, error) {
+	sl.l.Log("in SayGoodbye for " + userID)
+	name, ok := sl.ds.UserNameForID(userID)
+	if !ok {
+		return "", errors.New("unknown user")
+	}
+	return "Goodbye, " + name, nil
+}
+
+//when we want SimpleLogic instance, call a factory function, passing interfaces and returning struct
+func NewSimpleLogic(l Logger, ds DataStore) SimpleLogic {
+	return SimpleLogic{
+		l:  l,
+		ds: ds,
+	}
+}
+
+//our controller need business logic to says hello
+type Logic interface {
+	SayHello(userID string) (string, error)
+}
+
+type Controller struct {
+	l     Logger
+	logic Logic
+}
+
+func (c Controller) SayHello(w http.ResponseWriter, r *http.Request) {
+	c.l.Log("In SayHello")
+	userID := r.URL.Query().Get("user_id")
+	message, err := c.logic.SayHello(userID)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Write([]byte(message))
+}
+
+//controller factory function
+func NewController(l Logger, logic Logic) Controller {
+	return Controller{
+		l:     l,
+		logic: logic,
+	}
+}
