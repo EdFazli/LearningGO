e264c054163db932c96a6e17ef1ddfbcfa6df7e7
EdFazli <edfazli92@gmail.com>
2021-12-05 00:06:05 +0800

    adding ch7.1

---
+++ ch7.1/simpleWeb.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+//>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
+
+func main() {
+	fmt.Println("This is dependency injections example ")
+
+}
+
+//>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
