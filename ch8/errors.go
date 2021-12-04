7e72ce7b2e4f79e7fbe0f5e3b63f115dbf9139ce
EdFazli <edfazli92@gmail.com>
2021-12-05 01:12:38 +0800

    adding ch8

---
+++ ch8/errors.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+//>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
+
+func main() {
+	fmt.Println("This is errors examples")
+
+}
+
+//>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
