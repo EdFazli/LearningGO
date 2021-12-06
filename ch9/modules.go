82d471de7fc64feb5076197e460d277d17899e4e
EdFazli <edfazli92@gmail.com>
2021-12-07 01:44:41 +0800

    adding math and formatter

--- ch9/modules.go
+++ ch9/modules.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+
+	print "github.com/EdFazli/LearningGO/tree/main/ch1/ch9/formatter"
+	"github.com/EdFazli/LearningGO/tree/main/ch1/ch9/math"
 )
 
 //>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
@@ -9,6 +12,10 @@ import (
 func main() {
 	fmt.Println("This is modules, packages and imports examples")
 
+	//import example
+	num := math.Double(2)
+	output := print.Format(num)
+	fmt.Println(output)
 }
 
 //>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
