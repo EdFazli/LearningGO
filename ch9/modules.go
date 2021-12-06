064561e23a867a74e91a0fe2a5ec5652bbfe11e9
EdFazli <edfazli92@gmail.com>
2021-12-07 01:05:55 +0800

    adding ch9

---
+++ ch9/modules.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+//>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
+
+func main() {
+	fmt.Println("This is modules, packages and imports examples")
+
+}
+
+//>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
