e222ffd812477465ff803ab96d0d13f66dcd4258
EdFazli <edfazli92@gmail.com>
2021-12-12 01:26:05 +0800

    adding ch10

---
+++ ch10/concurrency.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+//>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
+
+func main() {
+	fmt.Println("This is concurrency examples")
+
+}
+
+//>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
