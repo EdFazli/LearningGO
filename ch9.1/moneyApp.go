9df86af88a9d46b46e317e05934c0d2fd35c18d2
EdFazli <edfazli92@gmail.com>
2021-12-11 23:23:27 +0800

    adding ch9.1

---
+++ ch9.1/moneyApp.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+//>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
+
+func main() {
+	fmt.Println("This is a small program that shows importing 3rd party code examples")
+
+}
+
+//>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
