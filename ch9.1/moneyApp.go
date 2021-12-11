c468de861c4a5610626f4cf9430cd2be86f2f411
EdFazli <edfazli92@gmail.com>
2021-12-12 00:14:56 +0800

    adding moneyApp.go

--- ch9.1/moneyApp.go
+++ ch9.1/moneyApp.go
@@ -2,12 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/learning-go-book/formatter"
+	"github.com/shopspring/decimal"
 )
 
 //>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
 
 func main() {
 	fmt.Println("This is a small program that shows importing 3rd party code examples")
+
+	if len(os.Args) < 3 {
+		fmt.Println("Need two parameters: amount and percent")
+		os.Exit(1)
+	}
+	amount, err := decimal.NewFromString(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	percent, err := decimal.NewFromString(os.Args[2])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	percent = percent.Div(decimal.NewFromInt(100))
+	total := amount.Add(amount.Mul(percent)).Round(2)
+
+	fmt.Println(formatter.Space(80, os.Args[1], os.Args[2], total.StringFixed(2)))
 
 }
 
--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.17
 require (
 	github.com/google/subcommands v1.0.1 // indirect
 	github.com/google/wire v0.5.0 // indirect
+	github.com/learning-go-book/formatter v0.0.0-20200921021027-5abc380940ae // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/shopspring/decimal v1.3.1 // indirect
 	golang.org/x/tools v0.0.0-20190422233926-fe54fb35175b // indirect
 )
