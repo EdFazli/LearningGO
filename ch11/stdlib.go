80225ea963b32da58f3ebd457ad40f97840bf2d3
EdFazli <edfazli92@gmail.com>
2021-12-15 02:41:19 +0800

    adding ch11

---
+++ ch11/stdlib.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+//>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
+
+func main() {
+	fmt.Println("This is a standard library examples")
+
+}
+
+//>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
