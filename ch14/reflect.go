1a09569cff25d3df11d20494164a5b590870f3b4
EdFazli <edfazli92@gmail.com>
2021-12-20 15:51:15 +0800

    adding ch14

---
+++ ch14/reflect.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+//>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
+
+func main() {
+	fmt.Println("This is a Reflect, Unsafe and Cgo examples")
+
+}
+
+//>>>>>>>>>>>>>>>>>>>>>>>>MAIN FUNCTION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
